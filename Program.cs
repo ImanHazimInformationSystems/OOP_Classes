00cb71ea66a755c407e59f4d742e6475495908e0
ImanHazimInformationSystems <130266573+ImanHazimInformationSystems@users.noreply.github.com>
2024-09-18 08:57:14 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //creating an object (Variable of a certain calss type)
+        Transaction transaction = new Transaction();
+        Date date = new Date();
+
+
+        transaction.month = 1;
+        transaction.val = 10.5F;
+
+        Console.WriteLine("Transaction value is " + transaction.val);
+    }
+}
+
